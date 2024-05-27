d89844e18bc6663a51b45ec19b4f94e6582fe368
Waqar Ali <57633448+WaqarAliBhatti@users.noreply.github.com>
2024-05-27 14:34:03 +0500

    Added Pass to swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT=process.env.PORT
   const config = new DocumentBuilder()
   .setTitle('My Practice App')
   .setDescription('The API description')
   .setVersion('1.0')
   .addBearerAuth() // Add Bearer Authentication configuration
   .build();
+  // set up password for accessing docs
+  // app.use(
+    // //Paths you want to protect with basic auth
+  //   '/docs*',
+  //   basicAuth({
+  //     challenge: true,
+  //     users: {
+  //       developer: 'Developer@123',
+  //     },
+  //   }),
+  // );
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('docs', app, document);
-  await app.listen(3000);
+  await app.listen(PORT||3002);
+  Logger.log(`App listening on port ${PORT}`);
+
 }
 bootstrap();
